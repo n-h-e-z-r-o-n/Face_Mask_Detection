93a5e4a2e9306d9e4efb97b52411df9d3f2ed0f3
Nangulu  Hezron Wekesa <55835551+Hezron26@users.noreply.github.com>
2022-12-12 18:52:08 +0300

    Add files via upload

--- Detect_From_Video.py
+++ Detect_From_Video.py
@@ -1,63 +1,51 @@
-from mtcnn import MTCNN
-import numpy as np
 import cv2
+import dlib
 import tensorflow as tf
 from keras.models import load_model
+import numpy as np
 
 model = load_model('model_detail/model/Face_Mask_Detection_Module.h5')
 labels = open('model_detail/model/labels.txt', 'r').readlines()
 
-detector = MTCNN()
-def draw_on_face(image, f_box, faces):
-    for i in range(len(f_box)):
-        x1 = f_box[i]['box'][0]
-        y1 = f_box[i]['box'][1]
-        x2 = f_box[i]['box'][2]
-        y2 = f_box[i]['box'][3]
-        predictions = model.predict(faces[i])
+vs = cv2.VideoCapture(0)# initialize the video stream and allow the camera sensor to warmup
+
+# initialize the HOG face detector
+face_detector = dlib.get_frontal_face_detector()
+
+def detect_mask(faces, frame):
+    # loop over the faces and draw a rectangle around each face
+    for face in faces:
+        x, y, w, h = face.left(), face.top(), face.right() - face.left(), face.bottom() - face.top()
+        face_crop = frame[y:y + h, x:x + w]
+        image_face = cv2.resize(face_crop, (224, 224), interpolation=cv2.INTER_AREA)  # Resize the raw image into (224-height,224-width) pixels.
+        image_face = np.asarray(image_face, dtype=np.float32).reshape(1, 224, 224, 3)  # Change the image into numpy array then reshape it to the models input shape.
+        image_face = (image_face / 127.5) - 1  # Normalize the image array
+
+        predictions = model.predict(image_face)
         score = tf.nn.softmax(predictions[0])
         Class = labels[np.argmax(score)]
         Confidence_score = int(100 * np.max(score))
         if Class[0] == '0':
-            cv2.rectangle(image, (x1, y1), (x1 + x2, y1 + y2), (0, 255, 0), 3)
-            cv2.putText(image, f'With MASK :{Confidence_score}%', (x1, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(frame, f'With MASK :{Confidence_score}%', (x, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
         if Class[0] == '1':
-            cv2.rectangle(image, (x1, y1), (x1 + x2, y1 + y2), (0, 0, 255), 3)
-            cv2.putText(image, f'Without MASK ::{Confidence_score}%', (x1, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            cv2.putText(frame, f'Without MASK ::{Confidence_score}%', (x, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
 
-def extract_faces(result_list, img):
-    face_s = []
-    box_s = []
-    for i in range(len(result_list)):
-        x1, y1, width, height = result_list[i]['box']
-        x1 = max(x1, 0)
-        y1 = max(y1, 0)
-        x2 = width + x1
-        y2 = height + y1
-        crop = img[y1:y1 + y2, x1:x1 + x2]  # img[y1:y2, x1:x2, :]
-        image_face = cv2.resize(crop, (224, 224), interpolation=cv2.INTER_AREA)  # Resize the raw image into (224-height,224-width) pixels.
-        image_face = np.asarray(image_face, dtype=np.float32).reshape(1, 224, 224, 3)  # Change the image into numpy array then reshape it to the models input shape.
-        image_face = (image_face / 127.5) - 1  # Normalize the image array
-        face_s.append(image_face)
-        box_s.append(result_list[i])
-    return box_s, face_s
-
-
-cap_video = cv2.VideoCapture(0)  # Capture video frame
-cap_video.set(3, 1080)
-cap_video.set(4, 920)
-offset = 20
 while True:
-    _, frame = cap_video.read()  # Read the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert each video frame to grayscale
-    face_boxes = detector.detect_faces(frame)
-    boxes, faces = extract_faces(face_boxes, frame)
-    draw_on_face(frame, boxes, faces)
-    cv2.imshow('Webcam Image', frame)  # Show the image in a window
-    keyboard_input = cv2.waitKey(1)
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
+    _, frame = vs.read() # grab the current frame
+    faces = face_detector(frame, 0) # detect faces in the frame
+    detect_mask(faces, frame)
+
+    # show the frame
+    cv2.imshow('Frame', frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord('q'):
         break
-cap_video.release()
+
+# do a bit of cleanup
+vs.release()
 cv2.destroyAllWindows()
--- User_Interface (GUI) .py
+++ User_Interface (GUI) .py
@@ -3,60 +3,44 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from keras.models import load_model
 import numpy as np
-from mtcnn import MTCNN
 import tensorflow as tf
+import dlib
 
 model = load_model('model_detail/model/Face_Mask_Detection_Module.h5')  # Load the model
 labels = open('model_detail/model/labels.txt', 'r').readlines()  # Grab the labels from the labels.txt file.
+face_detector = dlib.get_frontal_face_detector() # initialize the HOG face detector
+
 back_ground_color = 'white'  # UI background color
 
-detector = MTCNN()  # face detector
-root = tk.Tk()  # UI
-root.title("Face Mask Detection (group 3 : computer vision)")
-root.minsize(1000, 950)
-
-def draw_on_face(image, f_box, faces):
-    for i in range(len(f_box)):
-        x1 = f_box[i]['box'][0]
-        y1 = f_box[i]['box'][1]
-        x2 = f_box[i]['box'][2]
-        y2 = f_box[i]['box'][3]
-        predictions = model.predict(faces[i])
+def detect_mask(faces, frame):
+    # loop over the faces and draw a rectangle around each face
+    for face in faces:
+        x, y, w, h = face.left(), face.top(), face.right() - face.left(), face.bottom() - face.top()
+        try:
+            face_crop = frame[y:y + h, x:x + w]
+            image_face = cv2.resize(face_crop, (224, 224), interpolation=cv2.INTER_AREA)  # Resize the raw image into (224-height,224-width) pixels.
+            image_face = np.asarray(image_face, dtype=np.float32).reshape(1, 224, 224, 3)  # Change the image into numpy array then reshape it to the models input shape.
+            image_face = (image_face / 127.5) - 1  # Normalize the image array
+        except:
+            show_frame()
+        predictions = model.predict(image_face)
         score = tf.nn.softmax(predictions[0])
         Class = labels[np.argmax(score)]
         Confidence_score = int(100 * np.max(score))
-        if Confidence_score >= 60:
-            color = "Green"
-        else:
-            color = "red"
-        Probability_score.config(text=f"{Confidence_score} %", fg=color)
+        Probability_score.config(text=f"{Confidence_score} %")
         if Class[0] == '0':
-            cv2.rectangle(image, (x1, y1), (x1 + x2, y1 + y2), (0, 255, 0), 3)
-            cv2.putText(image, f'With MASK :{Confidence_score}%', (x1, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-            Face_Mask_status.config(text="Mask On", fg='Green')
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(frame, f'With MASK :{Confidence_score}%', (x, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+            Face_Mask_status.config(text='With MASK', fg='green')
         if Class[0] == '1':
-            cv2.rectangle(image, (x1, y1), (x1 + x2, y1 + y2), (0, 0, 255), 3)
-            cv2.putText(image, f'Without MASK ::{Confidence_score}%', (x1, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-            Face_Mask_status.config(text="No Mask On", fg='Brown')
-
-def extract_faces(result_list, img):
-    face_s = []
-    box_s = []
-    for i in range(len(result_list)):
-        x1, y1, width, height = result_list[i]['box']
-        x1 = max(x1, 0)
-        y1 = max(y1, 0)
-        x2 = width + x1
-        y2 = height + y1
-        crop = img[y1:y1 + y2, x1:x1 + x2]  # img[y1:y2, x1:x2, :]
-        image_face = cv2.resize(crop, (224, 224), interpolation=cv2.INTER_AREA)  # Resize the raw image into (224-height,224-width) pixels.
-        image_face = np.asarray(image_face, dtype=np.float32).reshape(1, 224, 224, 3)  # Change the image into numpy array then reshape it to the models input shape.
-        image_face = (image_face / 127.5) - 1  # Normalize the image array
-        face_s.append(image_face)
-        box_s.append(result_list[i])
-    return box_s, face_s
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+            cv2.putText(frame, f'Without MASK ::{Confidence_score}%', (x, y - 8), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            Face_Mask_status.config(text='Without MASK', fg='red')
 
 
+root = tk.Tk()  # UI
+root.title("Face Mask Detection (group 3 : computer vision)")
+root.minsize(1000, 950)
 
 content_frame = tk.Frame(root, bg=back_ground_color)
 content_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
@@ -66,7 +50,7 @@ Face_Mask_status = tk.Label(content_frame, bg=back_ground_color, anchor='w', fon
 Face_Mask_status.place(relheight=0.04, relwidth=0.25, rely=0.75, relx=0.251)
 
 tk.Label(content_frame, text='PROBABILITY SCORE ', bg=back_ground_color, anchor='e', font=('Consolas', 11, 'bold')).place(relheight=0.04, relwidth=0.23, rely=0.785, relx=0.02)
-Probability_score = tk.Label(content_frame, bg=back_ground_color, fg='white', anchor='w', text='with',font=('Courier New', 12, 'bold'))
+Probability_score = tk.Label(content_frame, bg=back_ground_color, fg='blue', anchor='w', font=('Courier New', 12, 'bold'))
 Probability_score.place(relheight=0.04,  relwidth=0.25, rely=0.785, relx=0.251)
 
 
@@ -79,12 +63,12 @@ v_Lable.place(relheight=1, relwidth=1, rely=0, relx=0)
 cap_video = cv2.VideoCapture(0)  # Capture video frame
 cap_video.set(3, 1080)
 cap_video.set(4, 920)
+
+
 def show_frame():
     _, frame = cap_video.read()  # Read the frame
-    face_boxes = detector.detect_faces(frame)
-    if face_boxes:
-        boxes, faces = extract_faces(face_boxes, frame)
-        draw_on_face(frame, boxes, faces)
+    faces = face_detector(frame, 0) # detect faces in the frame
+    detect_mask(faces, frame)
 
     # Display video frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -93,6 +77,7 @@ def show_frame():
     v_Lable.imgtk = imgtk
     v_Lable.configure(image=imgtk)
     v_Lable.after(5, show_frame)
+
 
 show_frame()
 root.mainloop()
